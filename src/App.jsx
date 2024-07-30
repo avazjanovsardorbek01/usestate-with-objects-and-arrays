a1979b496485b01811fe4c544d7dada89d54f5f9
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2024-07-30 16:07:16 +1000

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-md-6 offset-3 text-center">
+            <h1>Xatolikini to'girlang</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
